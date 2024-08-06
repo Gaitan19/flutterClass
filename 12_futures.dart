ab4015338df834df48f8b870c1972158c25ee002
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2024-08-06 16:06:22 -0600

    feat: Update httpGet function to use async/await for better readability and error handling

--- 12_futures.dart
+++ 12_futures.dart
@@ -1,19 +1,47 @@
-void main() {
+// void main() {
+//   print('Estamos a punto de pedir datos');
+
+//   httpGet('https://api.nasa.com/aliens').then((data) {
+//     print(data);
+//   }).catchError((err) {
+//     print('Error: $err');
+//   });
+
+//   print('Ultima linea');
+// }
+
+// Future<String> httpGet(String url) {
+//   return Future.delayed(const Duration(seconds: 1), () {
+//     throw 'No se pudo realizar la peticion';
+
+//     // return 'Respuesta de la peticion http';
+//   });
+// }
+
+// Async y Await
+
+void main() async {
   print('Estamos a punto de pedir datos');
 
-  httpGet('https://api.nasa.com/aliens').then((data) {
-    print(data);
-  }).catchError((err) {
-    print('Error: $err');
-  });
+  // httpGet('https://api.nasa.com/aliens').then((data) {
+  //   print(data);
+  // }).catchError((err) {
+  //   print('Error: $err');
+  // });
+
+  try {
+    final value = await httpGet('https://api.nasa.com/aliens');
+    print(value);
+  } catch (e) {
+    print('Error: $e');
+  }
 
   print('Ultima linea');
 }
 
-Future<String> httpGet(String url) {
-  return Future.delayed(const Duration(seconds: 1), () {
-    throw 'No se pudo realizar la peticion';
+Future<String> httpGet(String url) async {
+  await Future.delayed(const Duration(seconds: 1));
 
-    // return 'Respuesta de la peticion http';
-  });
+  throw 'No se pudo realizar la peticion';
+  // return 'Respuesta de la peticion http';
 }
--- 9_getterSetter.dart
+++ 9_getterSetter.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 void main() {
   final mySquare = Square(side: -5);
 
