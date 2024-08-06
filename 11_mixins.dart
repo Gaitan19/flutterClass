c0f272a66413bd634681328615c4f3abfab4d65c
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2024-08-06 15:22:00 -0600

    Add mixins for flying, walking, and swimming abilities

--- 11_mixins.dart
+++ 11_mixins.dart
@@ -5,3 +5,43 @@ abstract class Mamifero extends Animal {}
 abstract class Ave extends Animal {}
 
 abstract class Pez extends Animal {}
+
+mixin Volador {
+  void volar() => print('Estoy volando');
+}
+
+mixin Caminante {
+  void caminar() => print('Estoy caminando');
+}
+
+mixin Nadador {
+  void nadar() => print('Estoy nadando');
+}
+
+class Delfin extends Mamifero with Nadador {}
+
+class Murcielago extends Mamifero with Caminante, Volador {}
+
+class Gato extends Mamifero with Caminante {}
+
+class Paloma extends Ave with Volador, Caminante {}
+
+class Pato extends Ave with Volador, Nadador, Caminante {}
+
+class Tiburon extends Pez with Nadador {}
+
+class PezVolador extends Pez with Nadador, Volador {}
+
+void main() {
+  final pato = Pato();
+  pato.volar();
+  pato.nadar();
+  pato.caminar();
+
+  final pezVolador = PezVolador();
+  pezVolador.nadar();
+
+  final murcielago = Murcielago();
+  murcielago.caminar();
+  murcielago.volar();
+}
