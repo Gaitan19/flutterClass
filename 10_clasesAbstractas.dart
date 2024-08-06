c18eb6290b633319dd2c8ce7b22425fc1f411251
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2024-08-06 15:01:15 -0600

    feat: Add NuclearPlant class for energy generation

--- 10_clasesAbstractas.dart
+++ 10_clasesAbstractas.dart
@@ -1,6 +1,12 @@
 void main() {
   final windPlant = WindPlant(initialEnergy: 100);
   print(windPlant.energyLeft);
+
+  final nuclearPlant = NuclearPlant(energyLeft: 100);
+
+  print(nuclearPlant.energyLeft);
+  nuclearPlant.consumeEnergy(10);
+  print(nuclearPlant.energyLeft);
 }
 
 enum PlanType {
@@ -12,13 +18,14 @@ enum PlanType {
 
 abstract class EnergyPlant {
   double energyLeft;
-  PlanType type;
+  final PlanType type;
 
   EnergyPlant({required this.energyLeft, required this.type});
 
   void consumeEnergy(double amount);
 }
 
+// Extends
 class WindPlant extends EnergyPlant {
   WindPlant({required double initialEnergy})
       : super(energyLeft: initialEnergy, type: PlanType.wind);
@@ -26,5 +33,22 @@ class WindPlant extends EnergyPlant {
   @override
   void consumeEnergy(double amount) {
     energyLeft -= amount;
+  }
+}
+
+// Implement
+
+class NuclearPlant implements EnergyPlant {
+  @override
+  double energyLeft;
+
+  @override
+  final PlanType type = PlanType.nuclear;
+
+  NuclearPlant({required this.energyLeft});
+
+  @override
+  void consumeEnergy(double amount) {
+    energyLeft -= (amount * 0.5);
   }
 }
---
+++ 11_mixins.dart
@@ -0,0 +1,7 @@
+abstract class Animal {}
+
+abstract class Mamifero extends Animal {}
+
+abstract class Ave extends Animal {}
+
+abstract class Pez extends Animal {}
