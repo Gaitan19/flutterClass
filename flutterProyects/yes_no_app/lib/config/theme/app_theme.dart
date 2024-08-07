a0e83ef8be1dc799a7405895b284f1d216a9fd5f
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2024-08-07 16:19:20 -0600

    feat: Add AppTheme class for customizing the app's theme

---
+++ flutterProyects/yes_no_app/lib/config/theme/app_theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color(0xFF5c11d4);
+
+const List<Color> _colorsThemes = [
+  _customColor,
+  Colors.blue,
+  Colors.red,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.purple,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0,
+  }) : assert(selectedColor >= 0 && selectedColor <= _colorsThemes.length - 1,
+            '_ColorsThemes must be between 0 and ${_colorsThemes.length - 1}');
+
+  ThemeData theme() {
+    return ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: _colorsThemes[selectedColor],
+        brightness: Brightness.dark);
+  }
+}
