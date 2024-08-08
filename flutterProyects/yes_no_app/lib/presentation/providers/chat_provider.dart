91c291aee1883f9dc0ef6417668e9879818f6dfc
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2024-08-08 14:03:09 -0600

    added the scroll funcionality

--- flutterProyects/yes_no_app/lib/presentation/providers/chat_provider.dart
+++ flutterProyects/yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController ChatScrollController = ScrollController();
+
   List<Message> messageList = [
     Message(text: 'Hola', fromWho: FromWho.me),
     Message(text: 'que tal ?', fromWho: FromWho.me),
@@ -11,5 +13,14 @@ class ChatProvider extends ChangeNotifier {
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messageList.add(newMessage);
     notifyListeners();
+    moveScrollToBottom();
+  }
+
+  void moveScrollToBottom() {
+    ChatScrollController.animateTo(
+      ChatScrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
   }
 }
