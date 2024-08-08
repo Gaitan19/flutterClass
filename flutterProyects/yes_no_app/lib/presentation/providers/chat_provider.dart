abbd70ec3f8e3ba6132678b58dfe031d54d6dab8
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2024-08-08 13:44:51 -0600

    refactor: Update chat screen to use chatProvider.messageList for rendering messages

--- flutterProyects/yes_no_app/lib/presentation/providers/chat_provider.dart
+++ flutterProyects/yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  List<Message> messageList = [
     Message(text: 'Hola', fromWho: FromWho.me),
-    Message(text: 'que tal ?', fromWho: FromWho.him),
+    Message(text: 'que tal ?', fromWho: FromWho.me),
   ];
 
   Future<void> sendMessage(String text) async {}
