ae34f0eea46cedc8933b3933929b7566460476a0
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2024-08-09 14:50:08 -0600

    feat: Add RoundButton widget for displaying rounded buttons

--- flutterProyects/foot_restauran_app/lib/view/on_boarding/on_boarding_view.dart
+++ flutterProyects/foot_restauran_app/lib/view/on_boarding/on_boarding_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foot_restauran_app/common/color_extenstion.dart';
+import 'package:foot_restauran_app/common_widget/round_button.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
@@ -42,79 +43,94 @@ class _OnBoardingViewState extends State<OnBoardingView> {
 
   @override
   void initState() {
-    controller.addListener(() {
+    // TODO: implement initState
+    controller?.addListener(() {
       selectPage = controller?.page?.round() ?? 0;
-      if (mounted) setState(() {});
+      if (mounted) {
+        setState(() {});
+      }
     });
-
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     var media = MediaQuery.of(context).size;
-
     return Scaffold(
-        backgroundColor: TColor.primary,
-        body: SafeArea(
-          child: Stack(
+      backgroundColor: TColor.primary,
+      body: SafeArea(
+        child: Stack(children: [
+          PageView.builder(
+              controller: controller,
+              itemCount: infoArr.length,
+              itemBuilder: (context, index) {
+                var iObj = infoArr[index] as Map? ?? {};
+
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      iObj["icon"].toString(),
+                      width: media.width * 0.5,
+                      height: media.width * 0.5,
+                      fit: BoxFit.fill,
+                    ),
+                    SizedBox(
+                      height: media.width * 0.1,
+                    ),
+                    Text(
+                      iObj["title"].toString(),
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    SizedBox(
+                      height: media.width * 0.03,
+                    ),
+                    Text(
+                      iObj["sub_title"].toString(),
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700),
+                    )
+                  ],
+                );
+              }),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              PageView.builder(
-                  controller: controller,
-                  itemCount: infoArr.length,
-                  itemBuilder: (context, index) {
-                    var iObj = infoArr[index] as Map? ?? {};
+              RoundButton(
+                title: "Login",
+                onPressed: () {},
+              ),
+              SizedBox(
+                height: media.width * 0.1,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: infoArr.map((iObj) {
+                  var index = infoArr.indexOf(iObj);
 
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          iObj["icon"].toString(),
-                          width: media.width * 0.5,
-                          height: media.width * 0.5,
-                          fit: BoxFit.fill,
-                        ),
-                        SizedBox(height: media.width * 0.1),
-                        Text(iObj["title"].toString(),
-                            style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 24,
-                                fontWeight: FontWeight.w700)),
-                        SizedBox(height: media.width * 0.03),
-                        Text(iObj["sub_title"].toString(),
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w700)),
-                      ],
-                    );
-                  }),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: infoArr.map((iObj) {
-                      var index = infoArr.indexOf(iObj);
-
-                      return Container(
-                        margin: const EdgeInsets.all(8),
-                        width: 15,
-                        height: 15,
-                        decoration: BoxDecoration(
-                            color: selectPage == index
-                                ? Colors.white
-                                : Colors.white54,
-                            borderRadius: BorderRadius.circular(7.5)),
-                      );
-                    }).toList(),
-                  )
-                ],
+                  return Container(
+                      margin: const EdgeInsets.all(8),
+                      width: 15,
+                      height: 15,
+                      decoration: BoxDecoration(
+                          color: selectPage == index
+                              ? Colors.white
+                              : Colors.white54,
+                          borderRadius: BorderRadius.circular(7.5)));
+                }).toList(),
               )
             ],
-          ),
-        ));
+          )
+        ]),
+      ),
+    );
   }
 }
